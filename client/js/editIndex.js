710b7dad80142884983f74705ea285573caff82e
OmarLetona14 <omarletonaf@gmail.com>
2020-05-22 01:43:24 -0600

    Fixed bug with client

---
+++ client/js/editIndex.js
@@ -0,0 +1,36 @@
+function start(){
+
+    var texto = document.getElementById("txtA1").value;
+    console.log(texto);
+
+    var url='http://localhost:3000/api/analizador';
+
+   
+
+    $.post(url,{text:texto},function(data,status){
+        if(status.toString()=="success"){
+            alert("El resultado es: "+data.toString());
+            $('#jstree-tree').jstree("destroy");
+            arbol(data);
+        }else{
+            alert("Error estado de conexion:"+status);
+        }
+    });
+}
+
+function arbol(x){
+    var jsonData = 
+        JSON.parse(x);
+        ;
+        
+        $('#jstree-tree')
+          .on('changed.jstree', function (e, data) {
+            var objNode = data.instance.get_node(data.selected);
+            $('#jstree-result').html('Selected: <br/><strong>' + objNode.id+'-'+objNode.text+'</strong>');
+          })
+          .jstree({
+          core: {
+            data: jsonData
+          }
+        });
+}
